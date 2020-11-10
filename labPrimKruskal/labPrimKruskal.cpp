787510b70e5d9c29ce5e5d215a8a7df3b8c0c077
cbkf1 <cbkf12.ru@yandex.ru>
2020-11-11 01:16:14 +0300

    refactoring

--- labPrimKruskal/labPrimKruskal.cpp
+++ labPrimKruskal/labPrimKruskal.cpp
@@ -1,10 +1,8 @@
 ﻿// labPrimKruskal.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
-
 #include <iostream>
 #include <ctime>
 #include <vector> 
-
 #include "DHeap.cpp"
 using namespace std;
 
@@ -14,22 +12,11 @@ int** generateGrapth(int n, float d, int upperBoundWeight, vector<vector<int>>& 
     int maxCountOfEdge = n * (n - 1) / 2;
 
     int countOfEdge = int(d * maxCountOfEdge);
-
-    //int** grapthMatrix = new int* [n];
-    //int** minOstMatrix = new int* [n];
-
     int count = n;
 
     int currentEdgeCount = 0;
     vector<int> vartFree;
     vector<int> vertBind;
-
-    //vector<vector<int>> edgesStore;
-
-   // for (int i = 0; i < n; i++) {
-    //    grapthMatrix[i] = new int[n];
-    //    minOstMatrix[i] = new int[n];
-    //}
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
@@ -60,7 +47,6 @@ int** generateGrapth(int n, float d, int upperBoundWeight, vector<vector<int>>& 
             grapthMatrix[i][j] = 1;
             grapthMatrix[j][i] = 1;
             minOstMatrix[i][j] = minOstMatrix[j][i] = 1;
-            //int tmp[3] = { i, j, 1 };
             edgesStore.push_back({ i, j, 1 });
             int tmpBind = vartFree[indexI];
             auto iter = vartFree.cbegin();
@@ -112,8 +98,6 @@ int** generateGrapth(int n, float d, int upperBoundWeight, vector<vector<int>>& 
         }
         cout << '\n';
     }
-    
-    // return grapthMatrix, minOstMatrix;
     return grapthMatrix;
 }
 
@@ -161,34 +145,31 @@ void mspKruskal(vector<vector<int>>& ET, vector<vector<int>>& E, int n) {
         creation(p, r, i);
     }
     for (int i = 0; i < E.size(); i++) {
-        cout << "E[i][0]" << " " << E[i][0] << "    " << "ET[i][1]" << " " << E[i][1]<<"\n";
         int a = findRoot(p, r, E[i][0]);
         int b = findRoot(p, r, E[i][1]);
         if (a != b) {
             join(p, r, a, b);
             ET.push_back(E[i]);
-            //mt++;
         }
     }
     cout << "\n";
-    cout << "ANSWER" << "\n";
+    cout << "ANSWER KRUSKAL" << "\n";
     for (int i = 0; i < ET.size(); i++) {
-
-        cout << ET[i][2] << " ";
+        cout << ET[i][0] << " "<<ET[i][1] << " "<<ET[i][2] << " " << "\n";
     }
+
 }
 
 void mspPrim(int n, int** grapthMatrix) {
     vector<vector<DHeapItem>> grapth;
+    vector<DHeapItem> minTree;
     grapth.reserve(n);
     for (int i = 0; i < n; i++) {
         vector<DHeapItem> tmpVector;
         for (int j = 0; j < n; j++) {
-            // grapth.push_back({});
             if (grapthMatrix[i][j] > 0) {
                 DHeapItem tmp(i, j, grapthMatrix[i][j]);
-                tmpVector.push_back(tmp);
-                
+                tmpVector.push_back(tmp);   
             }
         }
         if (tmpVector.size() > 0) {
@@ -207,12 +188,9 @@ void mspPrim(int n, int** grapthMatrix) {
     int u = 0;
     used[u] = 1;
     for (int i = 0; i < grapth[u].size(); i++) {
-        //DHeapItem tmp(u, grapth[u][i].edgeSecond, grapth[u][i].weight);
-       // cout << grapth[u][i].edgeFirst << " " << grapth[u][i].edgeSecond << " " << grapth[u][i].weight << " " << "\n";
         heap.push(u, grapth[u][i].edgeSecond, grapth[u][i].weight);
     }
 
-    cout << "grapthsize " << grapth.size()<<" ";
     int mt = 0;
     while (mt<n-1)
     {
@@ -223,20 +201,20 @@ void mspPrim(int n, int** grapthMatrix) {
         }
         int w = tmp.weight;
         mt++;
-        cout << "\n";
-        cout << tmp.edgeFirst<<" "<<tmp.edgeSecond << " " << tmp.weight << " "<<"\n";
+        DHeapItem tmp1(tmp.edgeFirst, tmp.edgeSecond, tmp.weight);
+        minTree.push_back(tmp1);
         used[u] = 1;
-        cout << "- " << u;
         for (int i = 0; i < grapth[u].size(); i++) {
             if (used[grapth[u][i].edgeSecond] != 1) {
                 heap.push(u, grapth[u][i].edgeSecond, grapth[u][i].weight);
             }
-            //DHeapItem tmp(u, grapth[u][i].edgeSecond, grapth[u][i].weight);
-            
         }
-
     }
-
+    cout << "\n";
+    cout << "ANSWER PRIM" << "\n";
+    for (int i = 0; i < minTree.size(); i++) {
+        cout << minTree[i].edgeFirst << " " << minTree[i].edgeSecond << " " << minTree[i].weight << " " << "\n";
+    }
 }
 
 int main()
@@ -251,12 +229,8 @@ int main()
     cin >> d;
     cin >> UPPER;
     cout << "start generate grapth\n";
-    //int** grapthMatrix = new int*[N];
-    //int** minOstMatrix;
     int** grapthMatrix = new int* [N];
     int** minOstMatrix = new int* [N];
-
-    //vector<vector<int>> edgesStore;
 
     for (int i = 0; i < N; i++) {
         grapthMatrix[i] = new int[N];
@@ -270,32 +244,8 @@ int main()
 
     //** PRIM
 
-    /*vector<vector<DHeapItem>> grapth;
-    for (int i = 0; i < N; i++) {
-        for (int j = i; j < N; j++) {
-            if (grapthMatrix[i][j] > 0) {
-                DHeapItem tmp(i, j, grapthMatrix[i][j]);
-                grapth[i].push_back(tmp);
-            }
-        }
-    }*/
-
     mspPrim(N, grapthMatrix);
     system("pause");
     return 0;
 
 }
-
-// d-heap
-
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
